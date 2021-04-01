6dfbf137608b9ff888eae9dab44a556dd9dd2e49
Miká Kruschel <20423069+mikakruschel@users.noreply.github.com>
2021-04-01 18:09:51 +0200

    Save api keys via userdefaults

--- AC Widget/AC Widget/SettingsView.swift
+++ AC Widget/AC Widget/SettingsView.swift
@@ -6,16 +6,20 @@
 //
 
 import SwiftUI
+import WidgetKit
 
 struct SettingsView: View {
-    @AppStorage("issuerID") var issuerID = ""
-    @AppStorage("keyID") var keyID = ""
-    @AppStorage("apiKey") var apiKey = ""
-    @AppStorage("vendorNr") var vendorNr = ""
-    
+    @State var issuerID: String = UserDefaults.shared?.string(forKey: UserDefaultsKey.issuerID) ?? ""
+    @State var privateKeyID: String = UserDefaults.shared?.string(forKey: UserDefaultsKey.privateKeyID) ?? ""
+    @State var privateKey: String = UserDefaults.shared?.string(forKey: UserDefaultsKey.privateKey) ?? ""
+    @State var vendorNumber: String = UserDefaults.shared?.string(forKey: UserDefaultsKey.vendorNumber) ?? ""
+
     var body: some View {
         Form {
             keySection
+            
+            saveSection
+
             contactSection
         }
         .navigationTitle("Settings")
@@ -23,32 +27,62 @@ struct SettingsView: View {
     
     var keySection: some View {
         Section(header: Label("API Key", systemImage: "key.fill")) {
-            VStack(alignment: .leading) {
+            VStack(alignment: .leading, spacing: 0.0) {
                 Text("Issuer ID")
                     .bold()
                 
                 TextField("Issuer ID", text: $issuerID)
             }
             
-            VStack(alignment: .leading) {
-                Text("Key ID")
+            VStack(alignment: .leading, spacing: 0.0) {
+                Text("Private Key ID")
                     .bold()
                 
-                TextField("Key ID", text: $keyID)
+                TextField("Private Key ID", text: $privateKeyID)
             }
             
-            VStack(alignment: .leading) {
-                Text("Key")
+            VStack(alignment: .leading, spacing: 0.0) {
+                Text("Private Key")
                     .bold()
-                
-                TextEditor(text: $apiKey)
+                ZStack {
+                    TextEditor(text: $privateKey)
+                    if privateKey.isEmpty {
+                        VStack {
+                            HStack {
+                                Text("Private Key")
+                                    .foregroundColor(Color(UIColor.placeholderText))
+                                Spacer()
+                            }
+                        }
+                    }
+                }
             }
             
-            VStack(alignment: .leading) {
-                Text("Vendor Nr.")
+            VStack(alignment: .leading, spacing: 0.0) {
+                Text("Vendor Number")
                     .bold()
                 
-                TextField("Vendor Nr.", text: $vendorNr)
+                TextField("Vendor Number", text: $vendorNumber)
+            }
+        }
+    }
+    
+    var saveSection: some View {
+        Section {
+            Button("Save") {
+                UserDefaults.shared?.setValue(issuerID, forKey: UserDefaultsKey.issuerID)
+                UserDefaults.shared?.setValue(privateKeyID, forKey: UserDefaultsKey.privateKeyID)
+                UserDefaults.shared?.setValue(privateKey, forKey: UserDefaultsKey.privateKey)
+                UserDefaults.shared?.setValue(vendorNumber, forKey: UserDefaultsKey.vendorNumber)
+                
+                WidgetCenter.shared.reloadAllTimelines()
+            }
+            
+            Button("Cancel") {
+                issuerID = UserDefaults.shared?.string(forKey: UserDefaultsKey.issuerID) ?? ""
+                privateKeyID = UserDefaults.shared?.string(forKey: UserDefaultsKey.privateKeyID) ?? ""
+                privateKey = UserDefaults.shared?.string(forKey: UserDefaultsKey.privateKey) ?? ""
+                vendorNumber = UserDefaults.shared?.string(forKey: UserDefaultsKey.vendorNumber) ?? ""
             }
         }
     }
